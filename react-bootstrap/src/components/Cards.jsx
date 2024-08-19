3f6db23c824177f5522ac8038ac0b1d8082d5319
Sagar Raut <sagarraut2059@gmail.com>
2024-08-19 21:09:32 +0545

    update

--- react-bootstrap/src/components/Cards.jsx
+++ react-bootstrap/src/components/Cards.jsx
@@ -1,73 +1,72 @@
-import React from "react";
-import house from "../assets/houses/h1.jpg";
+import React, { useEffect, useState } from 'react'
+import house from '../assets/houses/h1.jpg'
 
 const Cards = () => {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-3">
-          <div className="card">
-            <img src={house} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3">
-          <div className="card">
-            <img src={house} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3">
-          <div className="card">
-            <img src={house} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3">
-          <div className="card">
-            <img src={house} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="https://www.facebook.com/" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+    // const articles = [
+    //     {
+    //         "source": {
+    //             "id": "345",
+    //             "name": "Google News"
+    //         },
+    //         "author": "Deadline",
+    //         "title": "Alain Delon Dies: Iconic French Actor Was 88 - Deadline",
+    //         "description": null,
+    //         "url": "https://news.google.com/rss/articles/CBMifkFVX3lxTE1haUlrbnFhaGF2SmQ0cmYxQXRWZjlGZlZnWFB2TGdmc0N3TzZPVFRfNGhPZ1psWHNLZnREazBPNzRIeFl3R2NOcjBFMUV4aVJic1U2bTJfNUJIZG1VeUMtU09CaVh5c3BuX1ZuLTdlWlVVQzlzWlpGb1ZwbF9IZw?oc=5",
+    //         "urlToImage": null,
+    //         "publishedAt": "2024-08-18T07:08:00Z",
+    //         "content": null
+    //     },
+    //     {
+    //         "source": {
+    //             "id": "123",
+    //             "name": "Google News"
+    //         },
+    //         "author": "Bleacher Report",
+    //         "title": "Dricus Du Plessis Beat Israel Adesanya Physically and Mentally at UFC 305 - Bleacher Report",
+    //         "description": null,
+    //         "url": "https://news.google.com/rss/articles/CBMiugFBVV95cUxOc2Rud1pGNmMtVVhMMUl0cHJfOWcwcVNpX0tjV0VkRGtvb1I1RTAzTzV1OXR1YXUzOXdhMGdkdUJ2ajdGWGRwZEVWMkdWYzlRNG9sVkRRR3VDdjdWUy13Y2tROUsxZE5ZRGtQZW54aGxFQW9LclZzLUdtTkdXT2lkRUg3c0I5cjVwaWZwb0JYUTFBNU9jaEcydXBQdzM1N0dSOTUxQUJ0WmFaVjlka1pTYnVhRkdHS1B1OEHSAc8BQVVfeXFMT3NYVW1OVFRNa2hXaTJlQVk4TGh4SV9UX1hVdnJ5aUZTRzBmclJKWms5SDBlbUxreHpHVHpIbThJN0U0ZEUtT21aSDdjTUFtNEMtZXJKaFNETTFKSkh3cUdrVWZRM1pLQ1dTVWpEblU3dDNpdDRaNjBlamhzTjR5MWY3aW5xc245SzBJUzFMX0xTa1NWUk05bXBIRjd3VGlXMlVzT3R1T3A5RGQzOGMySkR6cDc0Zkg0RHFiWGo1bWJJdURkTnU5RGswUlBmX1BJ?oc=5",
+    //         "urlToImage": null,
+    //         "publishedAt": "2024-08-18T06:02:11Z",
+    //         "content": null
+    //     }
 
-export default Cards;
+    // ]
+    const [article1, setArticle] = useState([])
+    //  console.log(article);
+    const fetchData =async () => {
+        let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=d125d26fbc6d49728775e0b977bddc5a"
+        let data = await fetch(url)
+        let parseData= await data.json()
+        console.log(parseData);
+        setArticle(parseData.articles)
+
+        
+       }
+       useEffect (()=>{
+        fetchData()
+       },[])
+    return (
+        <div className='container'>
+            <h4 className='service-heading my-3'>Our News</h4>
+            <div className='row'>
+                {article1 && article1.map((e) => {
+
+                    return (  // You need to add a return statement here
+                        <div className='col-md-3' key={e.source.id}>
+                            <div className="card" >
+                                <img src={e.urlToImage === null ? house : e.urlToImage} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">{e.title}</h5>
+                                    <p className="card-text">{e.source.id}</p>
+                                    <a href={e.url} target='blank' className="btn btn-primary">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Cards
--- react-bootstrap/src/components/Carousel.jsx
+++ react-bootstrap/src/components/Carousel.jsx
@@ -4,23 +4,32 @@ import house3 from '../assets/houses/h3.jpg'
 const Carousel = () => {
   return (
     <div>
-      <div
-        id="carouselExampleSlidesOnly"
-        className="carousel slide"
-        data-bs-ride="carousel"
-      >
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src={house1} className="d-block w-100" alt="..." />
-          </div>
-          <div className="carousel-item">
-            <img src={house2} className="d-block w-100" alt="..." />
-          </div>
-          <div className="carousel-item">
-            <img src={house3} className="d-block w-100" alt="..." />
-          </div>
-        </div>
-      </div>
+      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={house1} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={house2} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={house3} class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
     </div>
   );
 };
