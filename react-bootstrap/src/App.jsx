4e0e94bdfb266d173e8ff0f11d4598b7df07bf55
Sagar Raut <sagarraut2059@gmail.com>
2024-08-20 18:07:54 +0545

    react-router-dom

--- react-bootstrap/src/App.jsx
+++ react-bootstrap/src/App.jsx
@@ -1,44 +1,59 @@
-import { useState } from "react";
-import "./App.css";
-import Navbar from "./components/Navbar";
-import Alert from "./components/Alert";
-import Cards from "./components/Cards";
-import Carousel from "./components/Carousel";
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Alert from './components/Alert'
+import Cards from './components/Cards'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './components/Home'
 
 function App() {
-  const [mode, setMode] = useState("light");
-  const [text, setButtonText] = useState("Enable Dark");
-  const [alert, setAlert] = useState(null);
+  const apiKey = import.meta.env.VITE_API_KEY;
+  console.log('API Key:', apiKey);
+
+  const [mode, setMode] = useState('light')
+  const [text, setButtonText] = useState('Enable Dark')
+  const [alert, setAlert] = useState(null)
 
   const showAlert = (type, message) => {
     setAlert({
       type: type,
-      message: message,
-    });
+      message: message
+    })
     setTimeout(() => {
-      setAlert(null);
-    }, 1000);
-  };
+      setAlert(null)
+    }, 2000);
+
+  }
 
   const toggleMode = () => {
-    if (mode === "light") {
-      setMode("dark");
-      setButtonText("Enable light");
-      showAlert("success", "your dark mode has been enabled");
-    } else {
-      setMode("light");
-      setButtonText("Enable dark");
-      showAlert("success", "your light mode has been enabled");
+    if (mode === 'light') {
+      setMode('dark')
+      setButtonText('Enable light')
+      showAlert('success', 'your dark mode has been enabled')
     }
-  };
+    else {
+      setMode('light')
+      setButtonText('Enable dark')
+      showAlert('success', 'your light mode has been enabled')
+    }
+  }
   return (
     <>
-      <Navbar mode={mode} text={text} toggleMode={toggleMode} />
-      <Alert alert={alert} showAlert={showAlert} />
-      <Carousel />
-      <Cards />
+      <Router>
+        <Navbar mode={mode} text={text} toggleMode={toggleMode} />
+        <Alert alert={alert} showAlert={showAlert} />
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/blogs' element={<Cards apiKey={apiKey} />} />
+
+        </Routes>
+      </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- react-bootstrap/src/components/Navbar.jsx
+++ react-bootstrap/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = (props) => {
     return (
@@ -11,10 +12,10 @@ const Navbar = (props) => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
+                            <Link className="nav-link" to="/blogs">Blogs</Link>
                         </li>
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -27,9 +28,14 @@ const Navbar = (props) => {
                                 <li><a className="dropdown-item" href="#">Something else here</a></li>
                             </ul>
                         </li>
-                   
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+                        </li>
                     </ul>
-                
+                    {/* <form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form> */}
                     <button className='btn btn-primary' onClick={props.toggleMode}>{props.text}</button>
                 </div>
             </div>
