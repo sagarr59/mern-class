e5134239b3bd3dd26b8feae75171e87ad9fa6b39
Sagar Raut <sagarraut2059@gmail.com>
2024-08-30 19:23:16 +0545

    express-validator

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 //ES6 ma import... yaa require
 
 const mongoose = require("mongoose");
-const Mongo_URI = "mongodb://localhost:27017";
+const Mongo_URI = "mongodb://localhost:27017/mern-project";
 const dbConnect = () => {
   mongoose.connect(Mongo_URI).then(() => {
     console.log("mongoDB connected");
--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const User = require("../model/User");
+const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -9,10 +11,39 @@ router.get("/", (req, res) => {
   };
   res.send(mern);
 });
-router.post("/createuser", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
-  const user = User(req.body);
-  user.save();
-});
+router.post(
+  "/createuser",
+  [
+    body("name").isLength({ min: 3 }),
+
+    body("email").isEmail(),
+    // password must be at least 5 chars long
+    body("password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    // console.log(req.body);
+    // res.send(req.body);
+    // const user = User(req.body);
+    // user.save();
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    secPass = await bcrypt.hash(req.body.password, salt);
+
+    user = await User.create({
+      name: req.body.name,
+      username: req.body.email,
+      password: secPass,
+    }).then((user) => res.json(user));
+  }
+);
 module.exports = router;
